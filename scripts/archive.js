89be2de037d1fe198006e2da1789429e8b672f37
Alexandre B <contact@seldszar.fr>
2018-10-09 21:42:35 +0200

    chore: update deployment script

--- scripts/archive.js
+++ scripts/archive.js
@@ -2,9 +2,10 @@
 
 const archiver = require("archiver");
 const fs = require("fs");
+const { name } = require("../package.json");
 
 const archive = archiver("zip");
-const archiveFile = `dist/focal-v${process.argv[2]}.zip`;
+const archiveFile = `dist/${name}-v${process.argv[2]}.zip`;
 
 archive.directory("dist/", false);
 archive.pipe(fs.createWriteStream(archiveFile));
